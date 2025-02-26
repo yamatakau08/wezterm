9134f2f3689f1791bc49af25b4ec8894596fc3a8
yamatakau08 <yamatakau08@gmail.com>
2025-02-26 18:28:24 +0900

    first commit

---
+++ .wezterm.lua
@@ -0,0 +1,86 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+local act = wezterm.action
+
+-- This is where you actually apply your config choices
+
+-- For example, changing the color scheme:
+config.color_scheme = 'AdventureTime'
+
+config.enable_scroll_bar=true
+
+config.font = wezterm.font_with_fallback {
+  'MyricaM M',
+  'Myrica M',
+}
+config.font_size = 10.0
+
+config.initial_cols = 180
+config.initial_rows = 80
+
+if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
+   config.default_prog = { 'c:/cygwin64/bin/fish', '-l'}
+end
+
+config.window_background_opacity = 0.70
+
+config.keys = {
+  { key = 'l', mods = 'ALT', action = wezterm.action.ShowLauncher },
+  {
+    key = 't',
+    mods = 'ALT',
+    -- action = act.SpawnTab 'CurrentPaneDomain',
+    action = act.SpawnCommandInNewTab {
+       args = { 'cmd.exe' }
+    },
+  },
+}
+
+config.launch_menu = {
+  {
+    args = { 'top' },
+  },
+  {
+    -- Optional label to show in the launcher. If omitted, a label
+    -- is derived from the `args`
+    label = 'Bash',
+    -- The argument array to spawn.  If omitted the default program
+    -- will be used as described in the documentation above
+    args = { 'bash', '-l' },
+
+    -- You can specify an alternative current working directory;
+    -- if you don't specify one then a default based on the OSC 7
+    -- escape sequence will be used (see the Shell Integration
+    -- docs), falling back to the home directory.
+    -- cwd = "/some/path"
+
+    -- You can override environment variables just for this command
+    -- by setting this here.  It has the same semantics as the main
+    -- set_environment_variables configuration option described above
+    -- set_environment_variables = { FOO = "bar" },
+  },
+}
+
+-- https://wezterm.org/config/mouse.html#configuring-mouse-assignments#:~:text=delta
+config.mouse_bindings = {
+  -- Scrolling up while holding CTRL increases the font size
+  {
+    event = { Down = { streak = 1, button = { WheelUp = 1 } } },
+    mods = 'CTRL',
+    action = act.IncreaseFontSize,
+  },
+
+  -- Scrolling down while holding CTRL decreases the font size
+  {
+    event = { Down = { streak = 1, button = { WheelDown = 1 } } },
+    mods = 'CTRL',
+    action = act.DecreaseFontSize,
+  },
+}
+
+-- and finally, return the configuration to wezterm
+return config
